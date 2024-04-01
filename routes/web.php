565de3848db947585e4c44010f8859ccb740a249
HashLenox <hslenox0419@gmail.com>
2024-04-01 12:34:55 +0530

    Navigation layout created ✅

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
+Route::get('/home', function () {
+    return view('home');
+});
+
+
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
