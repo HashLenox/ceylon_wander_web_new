a9085ef0490bf71702be5d5ae7b0420c26d62253
HashLenox <hslenox0419@gmail.com>
2024-06-11 08:49:58 +0530

    travel card created ✅

--- resources/views/components/travel-img-slider.blade.php
+++ resources/views/components/travel-img-slider.blade.php
@@ -1,23 +1,21 @@
-<div class="mt-4 bg-white dark:bg-gray-900">
+<div class="bg-white  dark:bg-gray-900">
     <div id="default-carousel" class="relative w-full" data-carousel="slide">
         <!-- Carousel wrapper -->
-        <div class="relative h-56 overflow-hidden rounded-lg md:h-96">
+        <div class="relative h-56 overflow-hidden md:h-96">
             <!-- Item 1 -->
-            <div class="hidden duration-700 ease-in-out rounded-lg" data-carousel-item>
+            <div class="hidden duration-700 ease-in-out" data-carousel-item>
                 <img src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/hero/ngo-carousel/image-1.jpg"
-                    class="absolute block w-full -translate-x-1/2 -translate-y-1/2 rounded-lg top-1/2 left-1/2"
-                    alt="">
+                    class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="">
                 <div class="absolute inset-0 bg-gray-700 rounded-md opacity-60"></div>
                 <div class="absolute inset-0 flex items-center justify-center">
                     <h2 class="text-3xl font-bold text-white">Get Lost in Mountains</h2>
                 </div>
             </div>
             <!-- Item 2 -->
-            <div class="hidden duration-700 ease-in-out rounded-lg" data-carousel-item>
+            <div class="hidden duration-700 ease-in-out" data-carousel-item>
                 <img src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/hero/ngo-carousel/image-2.jpg"
-                    class="absolute block w-full -translate-x-1/2 -translate-y-1/2 rounded-lg top-1/2 left-1/2"
-                    alt="">
-                <div class="absolute top-0 left-0 w-full h-full bg-black rounded-lg opacity-50"></div>
+                    class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="">
+                <div class="absolute top-0 left-0 w-full h-full bg-black opacity-50"></div>
                 <div
                     class="absolute p-4 text-center text-white transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
                     <h2 class="text-xl font-semibold">Your Text Here</h2>
@@ -25,33 +23,30 @@
                 </div>
             </div>
             <!-- Item 3 -->
-            <div class="hidden duration-700 ease-in-out rounded-lg" data-carousel-item>
+            <div class="hidden duration-700 ease-in-out" data-carousel-item>
                 <img src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/hero/ngo-carousel/image-3.jpg"
-                    class="absolute block w-full -translate-x-1/2 -translate-y-1/2 rounded-lg top-1/2 left-1/2"
-                    alt="">
-                <div class="absolute top-0 left-0 w-full h-full bg-black rounded-lg opacity-50"></div>
+                    class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="">
+                <div class="absolute top-0 left-0 w-full h-full bg-black opacity-50"></div>
                 <div class="absolute left-0 p-4 text-center text-white top-10">
                     <h2 class="text-xl font-semibold">Your Text Here</h2>
                     <p class="text-sm">Additional text if needed</p>
                 </div>
             </div>
             <!-- Item 4 -->
-            <div class="hidden duration-700 ease-in-out rounded-lg" data-carousel-item>
+            <div class="hidden duration-700 ease-in-out" data-carousel-item>
                 <img src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/hero/ngo-carousel/image-4.jpg"
-                    class="absolute block w-full -translate-x-1/2 -translate-y-1/2 rounded-lg top-1/2 left-1/2"
-                    alt="">
-                <div class="absolute top-0 left-0 w-full h-full bg-black rounded-lg opacity-50"></div>
+                    class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="">
+                <div class="absolute top-0 left-0 w-full h-full bg-black opacity-50"></div>
                 <div class="absolute left-0 p-4 text-center text-white top-10">
                     <h2 class="text-xl font-semibold">Your Text Here</h2>
                     <p class="text-sm">Additional text if needed</p>
                 </div>
             </div>
             <!-- Item 5 -->
-            <div class="hidden duration-700 ease-in-out rounded-lg" data-carousel-item>
+            <div class="hidden duration-700 ease-in-out" data-carousel-item>
                 <img src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/hero/ngo-carousel/image-5.jpg"
-                    class="absolute block w-full -translate-x-1/2 -translate-y-1/2 rounded-lg top-1/2 left-1/2"
-                    alt="">
-                <div class="absolute top-0 left-0 w-full h-full bg-black rounded-lg opacity-50"></div>
+                    class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="">
+                <div class="absolute top-0 left-0 w-full h-full bg-black opacity-50"></div>
                 <div class="absolute left-0 p-4 text-center text-white top-10">
                     <h2 class="text-xl font-semibold">Your Text Here</h2>
                     <p class="text-sm">Additional text if needed</p>
